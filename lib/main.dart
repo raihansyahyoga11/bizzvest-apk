acf5c4f263461caa8eaa3e8538e08f1b87399958
nandhita.zefania <nandhita.zefania@ui.ac.id>
2021-12-24 23:35:36 +0700

    update faq

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
+// @dart=2.9
+
+import 'package:bizzvest/daftar_toko/main.dart';
+import 'package:bizzvest/faq/faq.dart';
+import 'package:bizzvest/halaman_toko/add_toko.dart';
 import 'package:bizzvest/halaman_toko/halaman_toko/halaman_toko.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   // runApp(const MyApp());
   // runApp(const HalamanTokoMaterial());
 
-  runApp(const HalamanTokoMaterialApp(id:8));
+  runApp(
+      MaterialApp(
+          home: FaqUtamaScreen()
+      )
+  );
 
- /* runApp(
+  /* runApp(
     DevicePreview(
       enabled: false,
       // enabled: !kReleaseMode,
